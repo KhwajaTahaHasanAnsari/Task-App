1546ae6e656fb58ff659db2dfbcc6c2ef82e3cec
KhwajaTahaHasanAnsari <ktahahasanansari@gmail.com>
2018-08-10 17:06:23 +0500

    ok

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::middleware(['auth'])->group(function() {
+    Route::resource('tasks', 'TaskController', [
+        'only' => [
+            'index', 'store', 'update'
+        ]
+    ]);
+
+});
